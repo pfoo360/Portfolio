0603708a86ecd844fa473b0841f63785ee2d92d6
Phillip Foo <pfoo360@gmail.com>
2023-03-18 10:46:41 -0400

    aesthetic changes to contact form

--- pages/index.tsx
+++ pages/index.tsx
@@ -175,7 +175,7 @@ export default function Home() {
           </div>
 
           <div className="flex flex-row justify-center w-full">
-            <div className="w-full max-w-[55rem] ">
+            <div className="w-full max-w-[40rem] ">
               <ContactForm />
             </div>
             <div className="hidden lg:inline-block absolute w-full max-w-[63rem] h-full max-h-[47.25rem] bottom-screen z-[-999]">
@@ -185,7 +185,8 @@ export default function Home() {
                 fill
                 className="opacity-90"
               />
-              <div className="absolute bottom-0 z-10 bg-gradient-to-t from-primary-black to-transparent h-[30vh] w-full" />
+              <div className="absolute -top-1 z-10 bg-gradient-to-b from-primary-black to-transparent h-[10vh] w-full" />
+              <div className="absolute bottom-0 z-10 bg-gradient-to-t from-primary-black to-transparent h-[15vh] w-full" />
               <div className="absolute z-10 -right-1 bg-gradient-to-l from-primary-black to-transparent h-full w-6/12" />
               <div className="absolute z-10 -left-1 bg-gradient-to-r from-primary-black to-transparent h-full w-6/12" />
             </div>
