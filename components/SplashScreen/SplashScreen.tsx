954a992171cdfbc968617024273ed8be60961bf5
Phillip Foo <pfoo360@gmail.com>
2023-10-19 13:54:21 -0400

    refactored code; moved splashscreen to its own separate component

---
+++ components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,27 @@
+import Blobs from "@/components/Blobs/Blobs";
+
+const SplashScreen = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-row justify-center items-start">
+      <div className="flex flex-col justify-center items-start h-full px-5 py-40 w-11/12 max-w-screen-xl">
+        <h1 className="font-roboto text-secondary-black text-[8rem] leading-none tracking-tight font-bold break-word text-justify uppercase">
+          HI,
+        </h1>
+        <h1 className="font-roboto text-secondary-black text-[8rem] leading-none tracking-tight font-bold break-word text-justify uppercase">
+          I AM
+        </h1>
+        <div className="absolute w-full max-w-xs top-96">
+          <Blobs />
+        </div>
+        <h1 className="font-roboto text-primary-purple text-[8.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase">
+          PHILLIP F.
+        </h1>
+        <h3 className="font-playfair text-white opacity-30 text-[1rem] leading-[1.5rem] tracking-[.1rem]">
+          SOFTWARE ENGINEER
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default SplashScreen;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import ContactForm from "@/components/ContactForm/ContactForm";
 import Link from "next/link";
 import GitHubLink from "@/components/GitHubLink/GitHubLink";
 import MiscLink from "@/components/MiscLink/MiscLink";
+import SplashScreen from "@/components/SplashScreen/SplashScreen";
 import Blobs from "@/components/Blobs/Blobs";
 import Navigation from "@/components/Navigation/Navigation";
 import { AiFillLinkedin } from "react-icons/ai";
@@ -34,25 +35,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <Navigation />
-      <div className="w-full min-h-screen flex flex-row justify-center items-start">
-        <div className="flex flex-col justify-center items-start h-full px-5 py-40 w-11/12 max-w-screen-xl">
-          <h1 className="font-roboto text-secondary-black text-[8rem] leading-none tracking-tight font-bold break-word text-justify uppercase">
-            HI,
-          </h1>
-          <h1 className="font-roboto text-secondary-black text-[8rem] leading-none tracking-tight font-bold break-word text-justify uppercase">
-            I AM
-          </h1>
-          <div className="absolute w-full max-w-xs top-96">
-            <Blobs />
-          </div>
-          <h1 className="font-roboto text-primary-purple text-[8.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase">
-            PHILLIP F.
-          </h1>
-          <h3 className="font-playfair text-white opacity-30 text-[1rem] leading-[1.5rem] tracking-[.1rem]">
-            SOFTWARE ENGINEER
-          </h3>
-        </div>
-      </div>
+      <SplashScreen />
 
       <div
         id="about"
