a630605bedd4bd8ccde115b5cb4b68fa64397e3f
Phillip Foo <pfoo360@gmail.com>
2023-10-19 14:22:34 -0400

    reordered imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import Navigation from "@/components/Navigation/Navigation";
 import SplashScreen from "@/components/SplashScreen/SplashScreen";
 import About from "@/components/About/About";
 import Skills from "@/components/Skills/Skills";
 import Work from "@/components/Work/Work";
 import Contact from "@/components/Contact/Contact";
-import Navigation from "@/components/Navigation/Navigation";
 
 //TODO: resume dl btn in about me section and contact section
 
