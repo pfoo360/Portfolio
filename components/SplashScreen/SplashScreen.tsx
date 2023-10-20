dd85b7c65bebff6c7c90ee334e38db5609765e80
Phillip Foo <pfoo360@gmail.com>
2023-10-19 22:10:20 -0400

    moved text up

--- components/SplashScreen/SplashScreen.tsx
+++ components/SplashScreen/SplashScreen.tsx
@@ -7,7 +7,7 @@ const SplashScreen = () => {
       className="w-full min-h-screen flex flex-row justify-center items-start bg-[#212028]"
     >
       <StarsCanvas />
-      <div className="flex flex-col justify-center items-center h-full px-5 py-40 w-11/12 max-w-screen-xl z-[10]">
+      <div className="flex flex-col justify-center items-center h-full px-5 py-32 w-11/12 max-w-screen-xl z-[10]">
         <h1 className="font-roboto text-primary-purple text-[8.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase">
           PHILLIP FOO
         </h1>
