de3c102c96ed1c79c8169db51910e189f86fc6e1
Phillip Foo <pfoo360@gmail.com>
2023-10-19 20:39:22 -0400

    changed splash screen from blobs to stars

--- components/SplashScreen/SplashScreen.tsx
+++ components/SplashScreen/SplashScreen.tsx
@@ -1,25 +1,18 @@
 import Blobs from "@/components/Blobs/Blobs";
+import StarsCanvas from "@/components/Stars/Stars";
 
 const SplashScreen = () => {
   return (
     <div
       id="home"
-      className="w-full min-h-screen flex flex-row justify-center items-start"
+      className="w-full min-h-screen flex flex-row justify-center items-start bg-[#212028]"
     >
-      <div className="flex flex-col justify-center items-start h-full px-5 py-40 w-11/12 max-w-screen-xl">
-        <h1 className="font-roboto text-secondary-black text-[8rem] leading-none tracking-tight font-bold break-word text-justify uppercase">
-          HI,
-        </h1>
-        <h1 className="font-roboto text-secondary-black text-[8rem] leading-none tracking-tight font-bold break-word text-justify uppercase">
-          I AM
-        </h1>
-        <div className="absolute w-full max-w-xs top-96">
-          <Blobs />
-        </div>
+      <StarsCanvas />
+      <div className="flex flex-col justify-center items-center h-full px-5 py-40 w-11/12 max-w-screen-xl z-[10]">
         <h1 className="font-roboto text-primary-purple text-[8.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase">
-          PHILLIP F.
+          PHILLIP FOO
         </h1>
-        <h3 className="font-playfair text-white opacity-30 text-[1rem] leading-[1.5rem] tracking-[.1rem]">
+        <h3 className="font-playfair text-white opacity-50 text-[1rem] leading-[1.5rem] tracking-[.1rem]">
           SOFTWARE ENGINEER
         </h3>
       </div>
---
+++ components/Stars/Stars.tsx
@@ -0,0 +1,54 @@
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as THREE from "three";
+
+const Stars = (props: ThreeElements["mesh"]) => {
+  const NUM_STARS = 512;
+  const ref = useRef(null!);
+  const [sphere] = useState(() => {
+    const len = NUM_STARS * 3;
+    const buf = new Float32Array(len);
+
+    for (let i = 0; i < len; i++) {
+      buf[i] = THREE.MathUtils.randFloatSpread(100);
+    }
+
+    return buf;
+  });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 15;
+    ref.current.rotation.y -= delta / 20;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f39dd6"
+          size={0.3}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-full absolute inset-0">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
