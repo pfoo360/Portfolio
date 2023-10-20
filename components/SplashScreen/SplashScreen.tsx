7b2feb0f78179b1a152dc49d52c1b2cd69fe98a0
Phillip Foo <pfoo360@gmail.com>
2023-10-19 23:31:32 -0400

    dynamic text display for splash screen; displays shortened name on smaller devices

--- components/SplashScreen/SplashScreen.tsx
+++ components/SplashScreen/SplashScreen.tsx
@@ -8,8 +8,17 @@ const SplashScreen = () => {
     >
       <StarsCanvas />
       <div className="flex flex-col justify-center items-center h-full px-5 py-32 w-11/12 max-w-screen-xl z-[10]">
-        <h1 className="font-roboto text-primary-purple text-[8.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase">
-          PHILLIP FOO
+        <h1 className="font-roboto text-primary-purple text-[8.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase sm:hidden">
+          PHIL
+        </h1>
+        <h1 className="font-roboto text-primary-purple text-[7.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase sm:hidden">
+          FOO
+        </h1>
+        <h1 className="font-roboto text-primary-purple text-[9.5rem] leading-none tracking-tight font-bold break-all text-justify uppercase hidden sm:block">
+          PHILLIP
+        </h1>
+        <h1 className="font-roboto text-primary-purple text-[11rem] leading-none tracking-tight font-bold break-all text-justify uppercase hidden sm:block">
+          FOO
         </h1>
         <h3 className="font-playfair text-white opacity-50 text-[1rem] leading-[1.5rem] tracking-[.1rem]">
           SOFTWARE ENGINEER
