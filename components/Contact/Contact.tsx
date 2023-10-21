490361e891feb14f208d3ec283191b44f580cd3f
Phillip Foo <pfoo360@gmail.com>
2023-10-21 17:10:42 -0400

    rm bg from contact section; added stalactites instead

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,17 +1,19 @@
 import ContactForm from "@/components/ContactForm/ContactForm";
 import GitHubLink from "@/components/GitHubLink/GitHubLink";
 import MiscLink from "@/components/MiscLink/MiscLink";
-import Image from "next/image";
+import Stalactites from "@/components/Stalactites/Stalactites";
 import { AiFillLinkedin } from "react-icons/ai";
+import Image from "next/image";
 import bg from "@/public/eberhard.jpg";
 
 const Contact = () => {
   return (
     <div
       id="contact"
-      className="w-full min-h-min flex flex-row justify-center items-start"
+      className="w-full min-h-min flex flex-row justify-center items-start bg-[#252028] relative"
     >
-      <div className="flex flex-col justify-start items-start h-full p-5 w-11/12 max-w-screen-xl">
+      <Stalactites />
+      <div className="flex flex-col justify-start items-start h-full p-5 w-11/12 max-w-screen-xl z-[10]">
         <h1 className="font-roboto text-primary-purple text-[4rem] leading-[6rem] tracking-tight font-bold uppercase">
           CONTACT
         </h1>
@@ -28,16 +30,9 @@ const Contact = () => {
           />
         </div>
 
-        <div className="flex flex-row justify-center w-full">
-          <div className="w-full max-w-[40rem] ">
+        <div className="flex flex-row justify-center w-full h-full">
+          <div className="w-full max-w-[40rem] h-full">
             <ContactForm />
-          </div>
-          <div className="hidden lg:inline-block absolute w-full max-w-[63rem] h-full max-h-[47.25rem] bottom-screen z-[-999]">
-            <Image src={`${bg.src}`} alt="bg" fill className="opacity-90" />
-            <div className="absolute -top-1 z-10 bg-gradient-to-b from-primary-black to-transparent h-[10vh] w-full" />
-            <div className="absolute bottom-0 z-10 bg-gradient-to-t from-primary-black to-transparent h-[15vh] w-full" />
-            <div className="absolute z-10 -right-1 bg-gradient-to-l from-primary-black to-transparent h-full w-6/12" />
-            <div className="absolute z-10 -left-1 bg-gradient-to-r from-primary-black to-transparent h-full w-6/12" />
           </div>
         </div>
       </div>
---
+++ components/Stalactites/Stalactites.tsx
@@ -0,0 +1,7 @@
+import Tites from "@/public/Tites.svg";
+
+const Stalactites = () => {
+  return <Tites className="w-full h-auto absolute top-0 aspect-[960/540]" />;
+};
+
+export default Stalactites;
